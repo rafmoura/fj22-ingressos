5862a8caaf01a3e60958e0cca1b4e981a41fb3ec
rafmoura <rafmoura25@gmail.com>
2018-09-05 19:23:27 -0300

    ajuste controller sessao

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,14 +1,18 @@
 package br.com.caelum.ingresso.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.FilmeDao;
+import br.com.caelum.ingresso.dao.SalaDao;
 
+
+@Controller
 public class SessaoController {
+	
 	@Autowired
 	private SalaDao salaDao;
 	
